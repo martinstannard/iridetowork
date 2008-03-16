965f02acce34a291cf35cbf41474a65782c2084a
Martin Stannard <mstannard@gmail.com>
2008-03-16 17:16:51 +1100

    make image an attachment_fu model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,19 @@
 class Image < ActiveRecord::Base
+
+  has_attachment :content_type => :image, 
+    :storage => :file_system,
+    :max_size => 1.megabytes,
+    :tempfile_path => "/tmp",
+    :thumbnails => { :huge => '400x400>', :large => '250x250>', :medium => '200x150>', :small => '120x90>', :tiny => '50x50>' }
+
+  validates_as_attachment
+
+  def self.parents
+    Image.find(:all, :conditions => ['parent_id IS NULL'])
+  end
+
+  def thumbs_by_size
+    thumbnails.sort_by { |t| t.width }
+  end
+
 end
